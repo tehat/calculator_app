d4e90e5dad100bf43838c773c4278b0e03d640e7
tehat <thomasehartle@gmail.com>
2015-11-02 08:27:22 -0600

    posting to DOM

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,7 +1,32 @@
 /**
  * Created by Thomas on 10/30/15.
  */
+
+
+//var mathFunctions = ["Add", "Subtract", "Multiply", "Divide"]
+var mathProblem = {};  //mathProblem is the object containing data(x,y) and type"Add,Subtract,Multiply,Divide"
+//var output = 0;
+
+
+
 $(document).ready(function(){
+
+    $("#functionButtons").on('click', '#add', function(){
+        console.log("yeh");
+        mathProblem["type"] = "Add";
+
+    });
+    $("#functionButtons").on('click', '#subtract', function(){
+        mathProblem["type"] = "Subtract";
+    });
+    $("#functionButtons").on('click', '#multiply',function(){
+        mathProblem["type"] = "Multiply";
+
+    });
+    $("#functionButtons").on('click', '#divide', function(){
+        mathProblem["type"] = "Divide";
+    });
+
 
     //$("#add").on('click', selectFunction);
     //$("#subtract").on('click', selectFunction);
@@ -15,59 +40,66 @@ $(document).ready(function(){
     //$("#multiply").on('click', function(){mathProblem["type"] = "Multiply"});
     //$("#divide").on('click', function(){mathProblem["type"] = "Divide"});
 
-    $("#getData").on('click', clickGetData);
+    //$("#getData").on('click', clickGetData);
     $("#postData").on('click', clickPostData);
 
 });
 
-function clickGetData(){
-    $.ajax({
-        type: "GET",
-        url: "/data",
-        success: function(data){
-            console.log(data);
-        }
-    });
-}
+//function clickGetData(){
+//    $.ajax({
+//        type: "GET",
+//        url: "/data",
+//        success: function(data){
+//            console.log(data);
+//        }
+//    });
+//}
 
 
 function clickPostData() {
 
-    var mathFunctions = ["Add", "Subtract", "Multiply", "Divide"]
-    var mathProblem = {};  //mathProblem is the object containing data(x,y) and type"Add,Subtract,Multiply,Divide"
-    //var output = 0;
+    //var mathFunctions = ["Add", "Subtract", "Multiply", "Divide"]
+    //var mathProblem = {};  //mathProblem is the object containing data(x,y) and type"Add,Subtract,Multiply,Divide"
+    ////var output = 0;
     $.each($("#inputForm").serializeArray(), function (i, field) {
         mathProblem[field.name] = field.value;
+        console.log("yep", mathProblem);
+
+
+    });
+
+
+
+
+
+        //mathProblem[field.type] = selectFunction(type);
         //$("#functionButtons").on('click', function(){
         //
         //
         //})
 
-        mathProblem["type"] = "Add";// !!!!!this works and I can create and object that has all the values that I need
+        //mathProblem["type"] = "Add";// !!!!!this works and I can create and object that has all the values that I need
         //I do not know how to connect the buttons to each individual function.
 
-        //$("#add").on('click', function(){mathProblem["type"] = "Add"});
-        //$("#subtract").on('click', function(){mathProblem["type"] = "Subtract"});
-        //$("#multiply").on('click', function(){mathProblem["type"] = "Multiply"});
-        //$("#divide").on('click', function(){mathProblem["type"] = "Divide"});
 
 
 
-    });
+
+
 
     //function selectFunction(type) {
     //    switch (type) {
-    //        case mathFunctions[0]:
-    //            type = "Add";
+    //        case ('#add'):
+    //            mathProblem.type = "Add";
     //            break;
     //        case mathFunctions[1]:
-    //            type = "Subtract";
+    //            mathProblem.type = "Subtract";
     //            break;
     //        case mathFunctions[2]:
-    //            mathProblem["type"] = "Multiply";
+    //            mathProblem.type = "Multiply";
     //            break;
     //        case mathFunctions[3]:
-    //            mathProblem["type"] = "Divide";
+    //            mathProblem.type = "Divide";
     //            break;
     //        default:
     //            text: "?";
@@ -87,6 +119,7 @@ function clickPostData() {
         },
         success: function (data) {
             console.log(data);
+            $('#outputForm').text(data.value);
 
         }
     });
